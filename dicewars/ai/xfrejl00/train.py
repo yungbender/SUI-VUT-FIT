92f43d11faa0bd36b7521e97ce68669a769829b6
TomasVenkrbec <xvenkr01@stud.fit.vutbr.cz>
2020-10-12 01:06:29 +0200

    changes to config.ini and argument loading

--- dicewars/ai/xfrejl00/train.py
+++ dicewars/ai/xfrejl00/train.py
@@ -7,6 +7,7 @@ import subprocess
 import re
 from datetime import datetime
 from configparser import ConfigParser
+from dicewars.ai.xfrejl00.qtable import QTable
 
 class TrainExc(Exception):
     pass
@@ -34,7 +35,7 @@ def fetch_machine():
     # Print relevant info about machine
     print("Using computer: " + os.uname()[1])
     print("Current time: " + datetime.now().strftime('%Y.%m.%d %H:%M:%S'))
-    print("Tensorflow version = " + tf.__version__)
+    print("Tensorflow version: " + tf.__version__)
     freeGpu = subprocess.check_output('nvidia-smi -q | grep "Minor\|Processes" | grep "None" -B1 | tr -d " " | cut -d ":" -f2 | sed -n "1p"', shell=True)
     if len(freeGpu) == 0:
         print('WARNING: No usable GPU available!')
@@ -52,22 +53,25 @@ def load_model(snapshot_path):
 
         dates.sort(reverse=True) # Sort by newest
         if len(dates) == 0:
-            print("ERROR: Can't find any folder with saved weights.")
+            print("ERROR: Can't find any folder with saved snapshot.")
             sys.exit(1)
         snapshot_path = dates[0] # Get the newest folder
 
     snapshot_path = "dicewars/ai/xfrejl00/snapshots/" + snapshot_path + "/"
 
-    if os.path.exists(snapshot_path) == False or os.path.isfile(snapshot_path + "saved_weights.h5") == False:
-        raise TrainExc("ERROR: Can't find file with saved weights.")
+    if os.path.exists(snapshot_path) == False or os.path.isfile(snapshot_path + "snapshot.pickle") == False:
+        raise TrainExc("ERROR: Can't find file with saved snapshot.")
 
     return snapshot_path
 
 
-def setup_snapshot_path(path):
+def setup_config(path, learning_rate, epsilon, discount):
     config = ConfigParser()
     config.read("dicewars/ai/xfrejl00/config.ini")
     config.set("BASE", "SnapshotPath", path)
+    config.set("BASE", "LearningRate", str(learning_rate))
+    config.set("BASE", "Epsilon", str(epsilon))
+    config.set("BASE", "Discount", str(discount))
     with open("dicewars/ai/xfrejl00/config.ini", "w") as configfile:
         config.write(configfile)
 
@@ -78,12 +82,11 @@ def train(matches_count=1000,
         snapshot_path=None,
         learning_rate=0.85, # Subject to change, will also decay during training
         epsilon=0.9, # Subject to change, will also decay during training
-        discount=0.99, # Subject to change
-        **kwargs):
+        discount=0.99): # Subject to change
+    setup_config(snapshot_path, learning_rate, epsilon, discount)
+    print("Snapshot path: " + snapshot_path)
     """
-    TODO: Start the game against AI's with new parameter signalizing that we're actively training the Q-table
-        - Change the game files to allow us to add new parameters controlling whether we're actively training, 
-            and will also let us switch the snapshot file location
+    TODO: Start the game against AI's
     TODO: Add snapshot saving
     TODO: Gather relevant data
         - Moving average of winrate against all anemies
@@ -97,12 +100,20 @@ def main():
     environment_setup()
     fetch_machine()
 
-    path = None
-    if args.load_model:
+    if args.load_model: # Load selected or latest snapshot
         path = load_model(args.dest_folder)
-    setup_snapshot_path(path)
+    else: # New snapshot
+        if args.dest_folder: # New snapshot with selected name
+            path = "dicewars/ai/xfrejl00/snapshots/" + args.dest_folder + "/"
+        else: # New snapshot with default name
+            path = "dicewars/ai/xfrejl00/snapshots/" + datetime.now().strftime('%Y.%m.%d %H:%M:%S') + "/"
+        os.makedirs(path)
 
-    train(**vars(args), snapshot_path=path)
+        q_table = QTable() # Create new Q-table
+        q_table.save(path + "snapshot.pickle")
+
+    args = dict((arg, getattr(args,arg)) for arg in vars(args) if getattr(args, arg) is not None) # Remove not empty args so we can get default values of arguments
+    train(**args, snapshot_path=path)
 
 if __name__ == "__main__":
     main()
