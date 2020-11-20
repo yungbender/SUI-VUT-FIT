fef9be5810d4eb109ad139fbbdb5a1fe355cb386
TomasVenkrbec <xvenkr01@stud.fit.vutbr.cz>
2020-11-20 22:56:36 +0100

    fixed old playable snapshot, adder newer one

--- dicewars/ai/alphadice-2.py
+++ dicewars/ai/alphadice-2.py
@@ -3,71 +3,47 @@ import random
 import pickle
 import numpy as np
 import copy
-import collections
+import os
+import signal
 from datetime import datetime
 from configparser import ConfigParser
 
 from dicewars.client.ai_driver import BattleCommand, EndTurnCommand
 from dicewars.ai.utils import save_state, possible_attacks, probability_of_successful_attack, probability_of_holding_area
-from dicewars.ai.xfrejl00.utils import region_size_potential_gain, convert_probability_to_classes
+from dicewars.ai.xfrejl00.qtable import QTable
+from dicewars.ai.xfrejl00.utils import *
+from dicewars.ai.xfrejl00.classifier import LogisticRegressionMultiFeature
 
-
-class QTableExc(Exception):
-    pass
-
-
-class QTablePickle(collections.UserDict):
-    def __init__(self, states_count=None, action_count=None, qvalue_check=False):
-        self.states_count = states_count
-        self.action_count = action_count
-        self.qvalue_check = qvalue_check
-
-        super().__init__(self)
-
-    def __getitem__(self, key: tuple):
-        if not isinstance(key, tuple):
-            raise QTableExc("Given key is not type tuple.")
-        elif len(key) != 2:
-            raise QTableExc("Accepting only keys in value ((state), (action))")
-
-        # Key[0] is state, Key[1] is action
-        if self.states_count is not None and len(key[0]) != self.states_count:
-            raise QTableExc("Given state in key does not have given number of states.")
-
-        if self.action_count is not None and len(key[1]) != self.action_count:
-            raise QTableExc("Given action in key does not have given number of states.")
-
-        return super().__getitem__(key)
-
-    @staticmethod
-    def load(where: str):
-        with open(where, "rb") as file:
-            return pickle.load(file)
+DROPOUT_RATE = 0.9 # How many dataset inputs will get dropped
+NB_FEATURES = 11 # Number of classifier features
 
 class AI:
     def __init__(self, player_name, board, players_order):
         self.player_name = player_name
         self.players_order = players_order
-
-        self.snapshot_path = "dicewars/ai/alphadice-1.pickle"
         
         self.logger = logging.getLogger('AI')
         self.logger.info("Current time: " + datetime.now().strftime('%Y.%m.%d %H:%M:%S'))
 
-        self.q_table = QTablePickle(states_count=3, action_count=1, qvalue_check=True)
-        self.q_table = self.q_table.load(self.snapshot_path)
+        self.q_table = QTable(states_count=5, action_count=1, qvalue_check=True)
+        self.q_table = self.q_table.load("dicewars/ai/alphadice-2.pickle")
 
     def get_qtable_key(self, board, source, target, action):
         # Get the individual states
         success_probability = probability_of_successful_attack(board, source.get_name(), target.get_name())
         hold_probability = probability_of_holding_area(board, target.get_name(), source.get_dice() - 1, self.player_name)
         region_gain = region_size_potential_gain(board, source.get_name(), target, self.player_name)
-
+        region_destroy = region_size_potential_destroy(board, source, target, self.player_name)
+        neighbor_count = neighboring_field_count(board, target)
+        
         # Transform the probability into class probability (very low, low, medium, high, very high)
         success_probability = convert_probability_to_classes(success_probability)
         hold_probability = convert_probability_to_classes(hold_probability)
+        region_gain = convert_region_difference_to_classes(region_gain)
+        region_destroy = convert_region_difference_to_classes(region_destroy)
+        neighbor_count = convert_neighbor_count_to_classes(neighbor_count)
  
-        return ((success_probability, hold_probability, region_gain), (action, ))
+        return ((success_probability, hold_probability, region_gain, region_destroy, neighbor_count), (action, ))
 
     def get_qtable_best_move(self, board, attacks):
         turn_source = None
@@ -86,7 +62,10 @@ class AI:
                         turn_action = action
         return turn_source, turn_target, turn_key, turn_action
 
-    def ai_turn(self, board, nb_moves_this_turn, nb_turns_this_game, time_left):
+
+    def ai_turn(self, board, nb_moves, nb_turns, time_left):
+        random.seed(random.random()) # New seed for an entire turn
+        
         turn_key = None
         attacks = list(possible_attacks(board, self.player_name))
         if attacks:
@@ -96,3 +75,6 @@ class AI:
             return EndTurnCommand()
         else:
             return BattleCommand(turn_source.get_name(), turn_target.get_name())
+
+    def save_training(self):
+        self.q_table.close()
